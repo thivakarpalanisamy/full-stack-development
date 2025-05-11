c0d3e0f1aafad8680f904d22defa854dabf07b87
thivakarpalanisamy <thivakarpalanisamy@outlook.com>
2025-05-11 21:08:24 +0530

    Added Multipage Project

---
+++ 07_MultiPage_Website/multipage_script.js
@@ -0,0 +1,12 @@
+document.getElementById("header").innerHTML = `<header>
+    <h1>🌐 My Universal Header</h1>
+    <nav>
+      <a href="multipage_index.html">Home</a> |
+      <a href="multipage_about.html">About Us</a> |
+      <a href="multipage_projects.html">Project</a>
+    </nav>
+  </header>`;
+
+document.getElementById("footer").innerHTML = `<footer>
+    SHAARATHI - THE APPRENTICE OF AI | &copy; copyrights | 2025
+</footer>`
